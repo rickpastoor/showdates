61872d2c3466c2e01a7dcf84ad82a1b992cfd3e1
Rick Pastoor <rick@blendle.com>
2017-05-14 13:18:13 +0200

    Add to be aired section to couch

--- lib/controllers/show.rb
+++ lib/controllers/show.rb
@@ -2,6 +2,15 @@ class ShowController < ShowdatesApp
   get '/:id' do
     @show = SDShow[params[:id]]
 
+    # Fetch the dataset we need
+    @episodes_dataset = SDEpisode.from_self(:alias => :episodes)
+      .left_join(SDUserEpisode, [Sequel.qualify(:episodes, :id) => :episode_id, :user_id => @user.id])
+      .join(:user_show, [Sequel.qualify(:user_show, :show_id) => Sequel.qualify(:episodes, :show_id), Sequel.qualify(:user_show, :user_id) => @user.id])
+      .join(SDSeason, {Sequel.qualify(:seasons, :id) => Sequel.qualify(:episodes, :season_id)}, :table_alias => :seasons)
+      .where(:episodes__show_id => params[:id])
+      .order(Sequel.qualify(:episodes, :show_id), Sequel.qualify(:seasons, :order), Sequel.qualify(:episodes, :order))
+      .select(:episodes__id, :episodes__title___episode_title, :user_episode__watched, :episodes__firstaired, :episodes__show_id, :episodes__season_id, :episodes__order, :seasons__title___season_title)
+
     erb :'show'
   end
 end
