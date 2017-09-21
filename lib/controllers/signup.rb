ca2412b99b04400189edaeae20efaea1f959ce9e
Rick Pastoor <rick@blendle.com>
2017-09-21 20:33:45 +0200

    Added some checks

--- lib/controllers/signup.rb
+++ lib/controllers/signup.rb
@@ -5,14 +5,29 @@ class SignupController < ShowdatesApp
 
   post '/' do
     # Check required fields
+    if params[:emailaddress].empty? || params[:password].empty? || params[:password_confirm].empty?
+      flash[:error] = "Please fill in the required fields!"
+
+      redirect '/signup'
+    end
 
     # Check username format
 
     # Check username availability
 
     # Check if this emailaddress is already in use
+    if SDUser.find(:emailaddress => params[:emailaddress])
+      flash[:error] = "This emailaddress is already taken. Do you want to <a href=\"/login\"><strong>login</strong></a> instead?"
+
+      redirect '/signup'
+    end
 
     # Check if the passwords match
+    if params[:password] != params[:password_confirm]
+      flash[:error] = "The passwords do not match."
+
+      redirect '/signup'
+    end
 
     salt = BCrypt::Engine.generate_salt
 
