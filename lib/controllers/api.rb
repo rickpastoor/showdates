69b101a1747387b44dce87f5df9c77d012f818b7
Rick Pastoor <rick@blendle.com>
2019-08-02 12:27:36 +0200

    Correct position

--- lib/controllers/api.rb
+++ lib/controllers/api.rb
@@ -23,6 +23,7 @@ class ApiController < ShowdatesApp
     result[:next_episode] = next_episode[:episode].to_hash
     result[:next_episode][:aired] = next_episode[:aired]
     result[:next_episode][:show_title] = episode.show.title
+    result[:next_episode][:timestamp] = next_episode[:episode].firstaired.to_time.to_i
     result[:next_episode][:firstaired_formatted] = next_episode[:episode].firstaired_formatted(current_date: @user.to_local_time(Time.now.getutc).to_date)
     result[:next_episode][:episode_code] = next_episode[:episode].episode_code
     result[:next_episode][:providerurl] = @user.providerurl_for_episode(next_episode[:episode])
